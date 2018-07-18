eebe31540186f42ba3b25eb559613c31e7bf1634
Manikanta <iiitmanikanta@users.noreply.github.com>
2018-07-18 12:37:38 +0530

    Update Popover.js

--- src/Popover.js
+++ src/Popover.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Overlay from 'react-overlays/lib/Overlay';
-import PropTypes from 'prop-types';
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+
+const _extends =
+  Object.assign ||
+  function (target) {
+    for (let i = 1; i < arguments.length; i++) {
+      const source = arguments[i]
+      for (const key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key]
+        }
+      }
+    }
+    return target
+  }
+
+const _react = require('react')
+
+const _react2 = _interopRequireDefault(_react)
+
+const _reactDom = require('react-dom')
+
+const _reactDom2 = _interopRequireDefault(_reactDom)
+
+const _Overlay = require('react-overlays/lib/Overlay')
+
+const _Overlay2 = _interopRequireDefault(_Overlay)
+
+const _propTypes = require('prop-types')
+
+const _propTypes2 = _interopRequireDefault(_propTypes)
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
 
 const PopoverStyle = {
   position: 'absolute',
   padding: '0 5px'
-};
+}
 
 const PopoverInnerStyle = {
   width: 200,
@@ -14,7 +47,7 @@ const PopoverInnerStyle = {
   color: '#666',
   backgroundColor: '#fff',
   boxShadow: '0 0 30px rgba(39, 43, 55, 0.15)'
-};
+}
 
 const PopoverArrowStyle = {
   position: 'absolute',
@@ -25,7 +58,7 @@ const PopoverArrowStyle = {
   borderTopColor: 'transparent',
   borderBottomColor: 'transparent',
   borderStyle: 'solid'
-};
+}
 
 const PlacementStyles = {
   left: {
@@ -56,7 +89,7 @@ const PlacementStyles = {
     }
   },
   bottom: {
-    tooltip: { marginBottom: 3, padding: '5px 0' },
+    tooltip: { marginBottom: 3, padding: '5px 0', marginLeft: -13 },
     arrow: {
       top: 0,
       marginLeft: -5,
@@ -64,69 +97,80 @@ const PlacementStyles = {
       borderBottomColor: '#fff'
     }
   }
-};
+}
 
-const PopoverContent = props => {
-  const placementStyle = PlacementStyles[props.placement];
+const PopoverContent = function PopoverContent(props) {
+  const placementStyle = PlacementStyles[props.placement]
 
-  const {
-    style,
-    innerStyle,
-    arrowOffsetLeft: left = placementStyle.arrow.left,
-    arrowOffsetTop: top = placementStyle.arrow.top,
-    children
-  } = props;
+  let style = props.style,
+    innerStyle = props.innerStyle,
+    _props$arrowOffsetLef = props.arrowOffsetLeft,
+    left =
+      _props$arrowOffsetLef === undefined
+        ? placementStyle.arrow.left
+        : _props$arrowOffsetLef,
+    _props$arrowOffsetTop = props.arrowOffsetTop,
+    top =
+      _props$arrowOffsetTop === undefined
+        ? placementStyle.arrow.top
+        : _props$arrowOffsetTop,
+    children = props.children
 
-  return (
-    <div style={{ ...PopoverStyle, ...placementStyle.tooltip, ...style }}>
-      <div
-        style={{ ...PopoverArrowStyle, ...placementStyle.arrow, left, top }}
-      />
-      <div style={{ ...PopoverInnerStyle, ...innerStyle }}>{children}</div>
-    </div>
-  );
-};
+  return _react2.default.createElement(
+    'div',
+    { style: _extends({}, PopoverStyle, placementStyle.tooltip, style) },
+    _react2.default.createElement(
+      'div',
+      { style: _extends({}, PopoverInnerStyle, innerStyle) },
+      children
+    )
+  )
+}
 
-const Popover = props => {
+const Popover = function Popover(props) {
   if (
-    ReactDOM.findDOMNode(props.container) &&
-    ReactDOM.findDOMNode(props.container).parentElement.parentElement !==
-      document.body
+    _reactDom2.default.findDOMNode(props.container) &&
+    _reactDom2.default.findDOMNode(props.container).parentElement
+      .parentElement !== document.body
   ) {
-    ReactDOM.findDOMNode(props.container).style.position = 'relative';
+    _reactDom2.default.findDOMNode(props.container).style.position = 'relative'
   }
-  return (
-    <Overlay
-      show={props.show}
-      onHide={props.onHide}
-      placement={props.placement}
-      container={props.container}
-      target={p => ReactDOM.findDOMNode(props.target)}
-      rootClose={props.hideWithOutsideClick}
-    >
-      <PopoverContent innerStyle={props.style} style={props.containerStyle}>
-        {props.children}
-      </PopoverContent>
-    </Overlay>
-  );
-};
+  return _react2.default.createElement(
+    _Overlay2.default,
+    {
+      show: props.show,
+      onHide: props.onHide,
+      placement: props.placement,
+      container: props.container,
+      target: function target(p) {
+        return _reactDom2.default.findDOMNode(props.target)
+      },
+      rootClose: props.hideWithOutsideClick
+    },
+    _react2.default.createElement(
+      PopoverContent,
+      { innerStyle: props.style, style: props.containerStyle },
+      props.children
+    )
+  )
+}
 
 Popover.propTypes = {
-  show: PropTypes.bool.isRequired,
-  onHide: PropTypes.func,
-  placement: PropTypes.string,
-  target: PropTypes.object,
-  style: PropTypes.object,
-  containerStyle: PropTypes.object,
-  container: PropTypes.object,
-  hideWithOutsideClick: PropTypes.bool,
-  children: PropTypes.element.isRequired
-};
+  show: _propTypes2.default.bool.isRequired,
+  onHide: _propTypes2.default.func,
+  placement: _propTypes2.default.string,
+  target: _propTypes2.default.object,
+  style: _propTypes2.default.object,
+  containerStyle: _propTypes2.default.object,
+  container: _propTypes2.default.object,
+  hideWithOutsideClick: _propTypes2.default.bool,
+  children: _propTypes2.default.element.isRequired
+}
 
 Popover.defaultProps = {
-  onHide: () => {},
+  onHide: function onHide() {},
   placement: 'bottom',
   hideWithOutsideClick: true
-};
+}
 
-export default Popover;
+exports.default = Popover
